00361cdf7cea8942a98cca619c3d3fbbef2a6e38
Pramod N <npramod95@gmail.com>
2020-02-15 17:36:59 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const App = () => {
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider, styled} from 'baseui';
+import { ButtonDesign } from './TestMe';
+const engine = new Styletron();
+const Centered = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+});
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Centered>
+          <ButtonDesign />
+        </Centered>
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
-
-export default App;
---
+++ src/TestMe.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Button } from "baseui/button";
+
+export const ButtonDesign = () => {
+  return (
+    <Button onClick={() => alert("click")}>Hello</Button>
+  );
+}
